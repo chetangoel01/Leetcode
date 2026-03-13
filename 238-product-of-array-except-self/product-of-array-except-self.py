9dcc943fa4627954b5605a2ca053067b8b605d93
Chetan Goel <85583636+chetangoel01@users.noreply.github.com>
2026-03-13 12:38:13 -0400

    Time: 31 ms (34.77%) | Memory: 27.9 MB (7.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # i remmeber this, this is a prefix sum type of question where i need left and right product
+        # if i skip the middle, the product at i will be left of i * right of i
+        # i need to calculate all left of i and right of i
+
+        left_product = [1]
+        for i in range(len(nums)):
+            left_product.append(left_product[-1] * nums[i])
+
+        # nums = [1, 2, 3, 4]
+        right_product = [1]
+        for i in range(len(nums)-1, -1, -1):
+            right_product.append(right_product[-1] * nums[i])
+        right_product.reverse()
+
+        result = []
+        for i in range(len(nums)):
+            result.append(left_product[i] * right_product[i+1])
         
-        zero_count = 0
-        zero_idx = -1
-        for i, n in enumerate(nums):
-            if n == 0: 
-                zero_count+=1
-                zero_idx = i
-            if zero_count > 1: return [0] * len(nums)
-
-        product = 1
-        for n in nums:
-            if n != 0:
-                product *= n
-
-        if zero_count:
-            # do something
-            answer = [0] * len(nums)
-            answer[zero_idx] = product
-        else:
-            answer = []
-            for n in nums:
-                    answer.append(int(product / n))
-
-        return answer
+        return result
